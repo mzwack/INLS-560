533e23c2c221c027a17e3b55d46cf29c7daae867
mzwack <mzwack@unc.edu>
2024-09-30 21:40:49 -0400

    Add files via upload

---
+++ Assignment_04.py
@@ -0,0 +1,7 @@
+items = ['sunflower', 'lily', 'tulip', 'rose']
+
+for item in items:
+    if item == "item3":
+        print(item.title()) # you may use any string method you wish.
+    else:
+        print(item.capitalize()) # you man use any string method you wish.
