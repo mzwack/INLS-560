8f4dc98475fc9b3c6ded2aa3da1e4411d302bb29
mzwack <mzwack@unc.edu>
2024-10-29 18:25:21 -0400

    Add files via upload

---
+++ amusement_park.py
@@ -0,0 +1,12 @@
+age = 101
+
+if age < 4:
+    print("Admission is $0")
+elif age < 18:
+    print("Admission is $25")
+elif age > 100: 
+    print("Admission is $0 and you get a free beer!")
+elif age >= 60:
+    print("Admission is $35")
+else:
+    print("Admission cost is $40")
